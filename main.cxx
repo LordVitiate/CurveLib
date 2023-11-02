ebca80de47d82f931d0fc0b394264199b0f45e17
root <root@p526971.kvmvps>
2023-11-02 00:07:14 +0000

    Fix container filling

--- main.cxx
+++ main.cxx
@@ -7,75 +7,76 @@
 #include <vector>
 
 
-using Container = std::vector<std::unique_ptr<ParametricCurve>>;
+using Element   = std::unique_ptr<ParametricCurve>;
+using Container = std::vector<Element>;
 
-class ContainerFabric {
-protected:
-    virtual void FillContainer(Container& container) = 0;
+class ElementFabric {
 public:
-    Container CreateContainer()
+    virtual Element MakeElement() = 0;
+};
+
+class HelixFabric : public ElementFabric 
+{
+protected:
+    Element MakeElement() override final
     {
-        Container container;
-
-        FillContainer(container);
-
-        return container;
+       return std::make_unique<Helix>(Point(rand() % 10, rand() % 10, rand() % 10), rand() % 5 + 1, 1);
     }
 };
 
-class HelixFabric : public ContainerFabric 
+class CircleFabric : public ElementFabric 
 {
 protected:
-    void FillContainer(Container& container) override final
+    Element MakeElement() override final
     {
-        for (int i = 0; i < 10; i++) container.push_back(std::make_unique<Helix>(Point(rand() % 10, rand() % 10, rand() % 10), rand() % 5 + 1, 1));
+		return std::make_unique<Circle>(Point(rand() % 10, rand() % 10, 0), rand() % 5 + 1);
     }
 };
 
-class CircleFabric : public ContainerFabric 
+class EllipseFabric : public ElementFabric 
 {
 protected:
-    void FillContainer(Container& container) override final
+    Element MakeElement() override final
     {
-		for (int i = 0; i < 10; i++) container.push_back(std::make_unique<Circle>(Point(rand() % 10, rand() % 10, 0), rand() % 5 + 1));
-    }
-};
-
-class EllipseFabric : public ContainerFabric 
-{
-protected:
-    void FillContainer(Container& container) override final
-    {
-		for (int i = 0; i < 10; i++) container.push_back(std::make_unique<Ellipse>(Point(rand() % 10, rand() % 10, 0), rand() % 5 + 1, rand() % 5 + 1));
+		return std::make_unique<Ellipse>(Point(rand() % 10, rand() % 10, 0), rand() % 5 + 1, rand() % 5 + 1);
     }
 };
 
 class RandomManner
 {
+private:
+	void FillContainer(ElementFabric& fabric, Container& container)
+	{
+		for (int i = 0; i < 2; i++) container.emplace_back(fabric.MakeElement());
+	}
 public:
 	Container CreateRandomContainer()
 	{
 		Container container;
-
-		int RandomVal = rand() % 3;
-        if(RandomVal == 0)
-        {
-			HelixFabric fabric; 
-			return fabric.CreateContainer();
-        }
-        if(RandomVal == 1)
-        {
-			CircleFabric fabric; 
-			return fabric.CreateContainer();
-        }
-        if(RandomVal == 2)
-        {
-			EllipseFabric fabric; 
-			return fabric.CreateContainer();
-        }
-        return container;
+		for (int i = 0; i < 10; ++i)
+		{
+			int RandomVal = rand() % 3;
+	        if(RandomVal == 0)
+	        {
+				HelixFabric fabric;
+				FillContainer(fabric,container);
+	        }
+	        if(RandomVal == 1)
+	        {
+				CircleFabric fabric; 
+				FillContainer(fabric,container);
+	        }
+	        if(RandomVal == 2)
+	        {
+				EllipseFabric fabric; 
+				FillContainer(fabric,container);
+	        }
+		}
+		return container; 
 	}	
 };
+
+
 
 void PrintContainer(const Container& container)
 {
@@ -88,17 +89,23 @@ void PrintContainer(const Container& container)
 		std::cout << " \t z: " << std::get<2>(fig->C(M_PI/4.0)) << " dz: " << std::get<2>(fig->dC(M_PI/4.0)) << std::endl;
 	}
 	std::cout << " ===== " << std::endl;
-
 }
+
 
 int main(int argc, char const *argv[])
 {
-	Container container;
 	RandomManner fabric;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 20; i++)
 	{
+		Container container;
 		container = fabric.CreateRandomContainer();
 		PrintContainer(container);
+		for(auto& fig :container) 
+		{
+			if (dynamic_cast<Circle*>(fig.get())!=nullptr) std::cout << "Circle" << std::endl;
+			if (dynamic_cast<Ellipse*>(fig.get())!=nullptr) std::cout << "Ellipse" << std::endl;
+			if (dynamic_cast<Helix*>(fig.get())!=nullptr) std::cout << "Helix" << std::endl;
+		}
 	}
 	return 0;
 }

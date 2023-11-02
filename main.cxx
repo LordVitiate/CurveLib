2a7dff83f60c52a664213623433b8a4b43d98ff2
root <root@p526971.kvmvps>
2023-11-02 02:04:49 +0000

    Sorting was added - Task5

--- main.cxx
+++ main.cxx
@@ -87,23 +87,36 @@ void PrintContainer(const Container& container)
 		std::cout << " \t x: " << std::get<0>(fig->C(M_PI/4.0)) << " dx: " << std::get<0>(fig->dC(M_PI/4.0)) << std::endl;
 		std::cout << " \t y: " << std::get<1>(fig->C(M_PI/4.0)) << " dy: " << std::get<1>(fig->dC(M_PI/4.0)) << std::endl;
 		std::cout << " \t z: " << std::get<2>(fig->C(M_PI/4.0)) << " dz: " << std::get<2>(fig->dC(M_PI/4.0)) << std::endl;
+		std::cout << " \t radius: " << fig->getRadius() << std::endl;
 	}
 	std::cout << " ===== " << std::endl;
 }
 
-
 int main(int argc, char const *argv[])
 {
+	auto is_circle = [](const auto& element) 
+	{
+    	return (std::dynamic_pointer_cast<Circle>(element) != nullptr);
+	};
+
+	auto compareCircles = [](const Element& first, const Element& second) 
+	{
+    	float radius1 = first->getRadius();
+   		float radius2 = second->getRadius();
+    	return radius1 < radius2;
+	};
+
 	RandomManner fabric;
 	for (int i = 0; i < 200; i++)
 	{
 		Container container;
 		container = fabric.CreateRandomContainer();
 		Container new_container;
-		std::copy_if(std::begin(container), std::end(container), std::back_inserter(new_container), [](const auto& element) 
-		{
-    		return (dynamic_cast<Circle*>(element.get()) != nullptr);
-		});
+
+		std::copy_if(std::begin(container), std::end(container), std::back_inserter(new_container), is_circle);
+
+		std::sort(std::begin(new_container), std::end(new_container), compareCircles);
+    
 		PrintContainer(new_container);
 
 	}
